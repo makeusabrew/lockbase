5726e7c231eb679ff0c6ddfe7d92fe7b82d0fade
Nick Payne <nick@kurai.co.uk>
2023-10-27 12:33:36 +0100

    strip readme

--- lib/admin/supabase-admin.ts
+++ lib/admin/supabase-admin.ts
@@ -1,11 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "../types_db";
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '../types_db'
 
 // TODO: line up with stripe: either make both functions, or make both just exports pre-initialised objects
 
 // TODO: possibly bring them into the same file
 export const createAdminClient = () =>
-  createClient<Database>(
-    process.env.SUPABASE_URL || "",
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
-  );
+  createClient<Database>(process.env.SUPABASE_URL || '', process.env.SUPABASE_SERVICE_ROLE_KEY || '')
