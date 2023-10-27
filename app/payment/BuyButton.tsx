06b6e548aeb0be8ded01605c4a7e51543377a547
Nick Payne <nick@kurai.co.uk>
2023-10-27 12:40:02 +0100

    inline stripe ID

--- app/payment/BuyButton.tsx
+++ app/payment/BuyButton.tsx
@@ -4,29 +4,26 @@ import { redirect } from 'next/navigation'
 
 const createCheckoutSession = async () => {
   'use server'
-  const priceId = process.env.STRIPE_PRICE_ID!
   const siteUrl = headers().get('origin')
   const session = await stripe.checkout.sessions.create({
     mode: 'payment',
     line_items: [
       {
-        price: priceId,
+        price: process.env.STRIPE_PRICE_ID,
         quantity: 1,
       },
     ],
     success_url: `${siteUrl}/payment?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${siteUrl}/`,
   })
-  console.log(`[STRIPE] Redirecting user to Stripe...`)
+  console.log(`Redirecting user to Stripe...`)
   redirect(session.url!)
 }
 
 export default function BuyButton() {
   return (
     <form action={createCheckoutSession}>
-      <button type="submit" className="btn btn-primary">
-        Buy now!
-      </button>
+      <button type="submit">Buy now!</button>
     </form>
   )
 }
